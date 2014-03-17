a1edcf608d50546ec7c3d9641bbfc2652e617bc3
Bojan Mihelac <bmihelac@mihelac.org>
2014-03-17 12:40:25 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+  '',
 #    url(r'^$', 'views.index', name='index'),
 )
