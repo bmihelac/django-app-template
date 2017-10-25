929d540a72dbd02b199aee8b02e16fa4b9c98553
Bojan Mihelac <bmihelac@mihelac.org>
2017-10-25 10:24:38 +0200

    Create test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test.testcases import TestCase
+
+
+class Test(TestCase):
+
+    def test_1(self):
+        self.assertEqual(1, 0)
